9eef29b3bcbab6e0fc0a650ed96ca40dfafb5139
Jagrutikatre27 <89924590+Jagrutikatre27@users.noreply.github.com>
2022-09-26 23:15:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+gsap.to("#bigimg", {
+    width: "65vw",
+    ease: "bounce",
+    duration: 1.5
+})
+gsap.to("#heading h1", {
+    left:"10%",
+    ease: Expo.ease,
+    duration: 2,
+    delay:.2
+})
+gsap.to("#box1", {
+    width:"25vh",
+    ease: "expo",
+    duration: 1,
+})
+gsap.to("#box1 h3", {
+    left:"20px",
+    ease: Expo.ease,
+    duration: 1,
+})
+gsap.to("#box2", {
+    width:"25.7vh",
+    ease: "expo",
+    duration: 1.5,
+})
+gsap.to("#box2inner h3", {
+    ease: Expo.ease,
+    duration: 1.5,
+})
+gsap.to("#box3", {
+    width:"25.7vh",
+    ease: "expo",
+    duration: 1.5,
+})
+gsap.to("#box3inner h3", {
+    ease: Expo.ease,
+    duration: 1.5,
+})
+gsap.to("#box4", {
+    width:"25vh",
+    ease: "expo",
+    duration: 1,
+})
+gsap.to("#box4 h3", {
+    right:"20px",
+    ease: Expo.ease,
+    duration: 1,
+})
+gsap.to("#middlebox", {
+    width:"25vh",
+    ease: "expo",
+    duration: 1,
+})
+gsap.to("#middlebox h3", {
+    ease: Expo.ease,
+    duration: 1,
+})
+gsap.to("#navbar", {
+    y:"20px",
+    ease: "bounce",
+    duration: 1,
+    delay:.2
+})
